b174dd9efa18a2495bd185fd52bc942bab01c5df
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-13 19:26:12 -0400

    Refactor AboutSection component and update styling

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,9 +1,25 @@
-
+import "./aboutsection.css";
 
 function AboutSection() {
   return (
-    <div>AboutSection</div>
-  )
+    <section className="aboutSection">
+      <div className="aboutContainer">
+        <div className="aboutInfo">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            Little Lemon is a charming neighborhood bistro that serves simple
+            food and classic cocktails in a lively but casual environment. The
+            restaurant features a locally sourced menu with daily specials.
+          </p>
+        </div>
+        <div className="aboutImgs">
+          <img id="restImg" src="/images/restaurant.jpg" alt="restaurant" />
+          <img id="chefImg" src="/images/chef.jpg" alt="chef" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default AboutSection
+export default AboutSection;
