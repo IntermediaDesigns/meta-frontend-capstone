a3354a5a107cb3da5778b98fdb1545bf617ffbd4
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-13 19:43:04 -0400

    Refactor testimonials component and update testimonials content

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import './testimonials.css';
+import "./testimonials.css";
 function Testimonials() {
   return (
     <section className="testimonials">
@@ -7,26 +7,31 @@ function Testimonials() {
         <div className="testimonialBox">
           <img src="/images/stars.png" alt="" />
           <p>
-            “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+            “Little Lemon&rsquo;s authentic Mediterranean cuisine tantalized my
+            taste buds with its vibrant flavors and fresh ingredients. The cozy
+            atmosphere and friendly staff made my dining experience truly
+            memorable.”
           </p>
-          <h3>~ John Doe</h3>
+          <h3>~ Jake Johnson</h3>
         </div>
         <div className="testimonialBox">
           <img src="/images/stars.png" alt="" />
           <p>
-            “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+            “The atmosphere at Little Lemon is simply enchanting. The warm
+            lighting, soft music, and aromatic scents create an inviting and
+            intimate setting that perfectly complements the delicious food.”
           </p>
-          <h3>~ John Doe</h3>
+          <h3>~ Carol Sydney</h3>
         </div>
         <div className="testimonialBox">
           <img src="/images/stars.png" alt="" />
           <p>
-            “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+            “More than just a meal, dining at Little Lemon is an immersive
+            culinary journey. The attentive service and the chef&rsquo;s passion for
+            Mediterranean flavors made my dining experience an unforgettable
+            one.”
           </p>
-          <h3>~ John Doe</h3>
+          <h3>~ Lisa Davis</h3>
         </div>
       </div>
     </section>
